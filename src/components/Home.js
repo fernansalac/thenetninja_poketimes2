6b534db13fd367e4c17aa235f31dc32a34d20228
Fernan Salac <salacfernan@gmail.com>
2020-05-01 22:20:43 +0800

    route param2

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,16 +1,17 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import { Link } from 'react-router-dom';
 
 class Home extends Component {
     state = {
-        posts: [ ]
+        posts: []
     }
     componentDidMount() {
         axios.get('https://jsonplaceholder.typicode.com/posts')
             .then(res => {
                 console.log(res);
                 this.setState({
-                    posts: res.data.slice(1, 10)
+                    posts: res.data.slice(0, 10)
                 })
             })
     }
@@ -22,7 +23,9 @@ class Home extends Component {
                 return (
                     <div className="post card" key={post.id} >
                         <div className="card-content">
-                            <span className="card-title">{post.title}</span>
+                            <Link to={'/' + post.id} >
+                                <span className="card-title">{post.title}</span>
+                            </Link>
                             <p>{post.body}</p>
                         </div>
                     </div>
@@ -34,7 +37,7 @@ class Home extends Component {
         return (
             <div className="container">
                 <h4 className="center">Home</h4>
-                { postList}
+                {postList}
             </div>
         )
     }
